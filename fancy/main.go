0ea9fde1522c1e9b61e69bb1ac6d88f44085adc2
Victor Iheanacho <victor@victors-MacBook-Pro.local>
2020-05-04 22:38:09 +0100

    --amend

--- fancy/main.go
+++ fancy/main.go
@@ -5,6 +5,8 @@ import (
 	"github.com/victor-nach/go-microservices-test/fancy/fan"
 )
 
+var Lami = "anagrma"
+
 func main() {
 	fmt.Println("starting something main service..")
 	fmt.Println("from some package: ", fan.SomeVariable)
--- something/main.go
+++ something/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/victor-nach/go-microservices-test/something/some"
+	// "github.com/victor-nach/go-microservices-test/something/some"
 )
 
 func main() {
 	fmt.Println("starting something main service..")
-	fmt.Println("from some package: ", some.SomeVariable)
+	// fmt.Println("from some package: ", some.SomeVariable)
 }
