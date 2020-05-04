4cd8a51afa04d00580cf9eb60990f7725ce910b6
Victor Iheanacho <victor@victors-MacBook-Pro.local>
2020-05-04 22:13:30 +0100

    update module path

--- user/go.mod
+++ user/go.mod
@@ -1,3 +1,5 @@
 module users
 
 go 1.13
+
+require github.com/victor-nach/go-microservices-test v0.0.0-20200504210837-8c34d758b439 // indirect
--- user/main.go
+++ user/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/victor-nach/go-microservices-test/something"
 )
 
 func main() {
 	fmt.Println("starting user service..")
+	fmt.Println("from some package: ", some.SomeVariable)
 }
