ff1dfcbbc6f92c9513c311cf7f391029361be8a6
Victor Iheanacho <victor@victors-MacBook-Pro.local>
2020-05-04 22:37:50 +0100

    --amend

--- user/main.go
+++ user/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/victor-nach/go-microservices-test/something"
+	// "github.com/victor-nach/go-microservices-test/something"
+	"github.com/victor-nach/go-microservices-test/fancy/fan"
 )
 
 func main() {
 	fmt.Println("starting user service..")
-	fmt.Println("from some package: ", some.SomeVariable)
+	fmt.Println("from some package: ", fan.SomeVariable)
 }
